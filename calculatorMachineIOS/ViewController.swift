28bcc4938ec260fbfa15a30b110392f0da65bac1
burak-cakir <burakcakir@hotmail.com.tr>
2021-11-10 15:34:09 +0300

    sum button ok

--- calculatorMachineIOS/ViewController.swift
+++ calculatorMachineIOS/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var equalNumber: UILabel!
+    @IBOutlet weak var secondNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sumButton(_ sender: Any) {
+        let firstNum = Int( firstNumber.text!)!
+        let secondNum = Int(secondNumber.text!)!
+        equalNumber.text = String(firstNum + secondNum)
+        
+    }
+    
+    @IBAction func subButton(_ sender: Any) {
+    }
+    @IBAction func divideButton(_ sender: Any) {
+    }
+    @IBAction func multiplicationButton(_ sender: Any) {
+    }
 }
 
